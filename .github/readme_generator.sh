65dfcad0a8d3e915f40deb01b9f1af0c8cdfd67d
sethpoly <emailspolyniak@gmail.com>
2021-12-31 11:09:31 -0500

    [main] - add . notation

--- .github/readme_generator.sh
+++ .github/readme_generator.sh
@@ -2,7 +2,7 @@
 echo -e "# Github Aliases \n
 Just a collection of github aliases that I found useful \n
 \`\`\`
-$(cat ${GITHUB_WORKSPACE}/alias | less)
+$(cat ./alias | less)
 \`\`\` 
 Last updated: $(date)
 " 
