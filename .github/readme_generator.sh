270bc43aa72b07fad00fe6416a4e999cccb3e3ec
sethpoly <emailspolyniak@gmail.com>
2021-12-31 10:53:58 -0500

    [main] - update dir

--- .github/readme_generator.sh
+++ .github/readme_generator.sh
@@ -2,7 +2,7 @@
 echo -e "# Github Aliases \n
 Just a collection of github aliases that I found useful \n
 \`\`\`
-$(cat ../alias | less)
+$(cat ${GITHUB_WORKSPACE}/alias | less)
 \`\`\` 
 Last updated: $(date)
 " > ../README.md
