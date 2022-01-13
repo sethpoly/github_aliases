942adfa1f6230c2e3a66b680bfd2496f2995021a
sethpoly <emailspolyniak@gmail.com>
2022-01-13 08:59:31 -0500

    [main] - add instructions to readme

--- .github/readme_generator.sh
+++ .github/readme_generator.sh
@@ -4,5 +4,10 @@ Just a collection of github aliases that I found useful \n
 \`\`\`
 $(cat ./alias | less)
 \`\`\` 
+## How to use
+- Clone this repo wherever you desire
+- In your \`~/.bash_profile\`, add \`source PATH/TO/github_aliases/alias\`
+- In a fresh shell session, \`cd ~\`, then \`source .bash_profile\`
+
 Last updated: $(date)
 " 
